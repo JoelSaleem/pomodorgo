7ddbbba8414dfd0f2ddcf7f6461aab6645d7543c
JoelSaleem <joeljsaleem@gmail.com>
2023-08-14 08:48:43 +0100

    :bulb: todos

--- internal/content/tasks/tasks.go
+++ internal/content/tasks/tasks.go
@@ -1,5 +1,11 @@
 package tasks
 
+// add task (persistent) -- title, description, due date, priority, status
+// soft delete task (persistent)
+// update task (persistent)
+// view tasks
+// archive task -- not shown in list
+// unarchive task
 type Tasks struct {}
 
 func NewTasks() *Tasks {
